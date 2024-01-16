3b4055c67c2b20a2830dfa7a215b9c4ce8f3d748
Yan Araujo Resende <saresende555@gmail.com>
2024-01-16 15:46:01 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/yanresende.png" />
-    <Titulo fontSize={20}>Yan Resende</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Yan Resende</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        yanresende
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
